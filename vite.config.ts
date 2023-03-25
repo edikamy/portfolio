6c825725bc362350bf7aa0978f08f6b458c832d3
kambiz <kambiz@queensite.eu>
2023-03-25 18:21:52 +0330

    feat(admin): add compress image to vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,30 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/portfolio/',
-  plugins: [react()],
+  plugins: [
+    react(),
+    ViteImageOptimizer({
+      png: { quality: 70, compressionLevel: 9 },
+      jpeg: { quality: 70, compressionLevel: 9 },
+      jpg: { quality: 70, compressionLevel: 9 },
+      gif: { quality: 70, compressionLevel: 9 },
+      svg: { quality: 70, compressionLevel: 9 },
+    }),
+  ],
   build: {
     rollupOptions: {
       output: {
         manualChunks: {
           threeJs: ['@react-three/drei', '@react-three/fiber', 'three', 'react-tilt'],
+        },
+        minifyInternalExports: true,
+        compact: true,
+        chunkFileNames(chunkInfo) {
+          return `Kambiz-Chunk-${chunkInfo.name}.js`
         },
       },
     },
